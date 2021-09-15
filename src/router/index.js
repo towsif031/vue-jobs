9fc506d90ba2e5f0f507c5e9dca76b924bf241b9
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-16 00:02:53 +0600

    separate folder for related route components.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
-import Jobs from '../views/Jobs.vue';
+import Jobs from '../views/jobs/Jobs.vue';
 
 const routes = [
 	{
