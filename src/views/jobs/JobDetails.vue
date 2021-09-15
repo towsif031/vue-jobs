bde2c9c19d150a1cf2bd5b26b5049c8934961127
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-16 00:36:56 +0600

    route parameters.

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,16 @@
+<template>
+	<h1>Job Details Page</h1>
+	<p>The job id is {{ id }}</p>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			id: this.$route.params.id
+		};
+	}
+};
+</script>
+
+<style></style>
