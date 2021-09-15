e0a6305002f93468a7ca4c1aa02c46960c5db766
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-15 23:35:54 +0600

    cleaned up

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eos
+			veritatis rerum. Vero, cupiditate itaque ratione non officia
+			consectetur iusto expedita impedit. Culpa officiis tempora nulla
+			excepturi, ipsum quos odio, harum cum cumque neque consequuntur
+			dicta obcaecati. Eaque totam adipisci dolore explicabo nulla nostrum
+			labore veritatis mollitia nobis dolorum ducimus, non ut vero
+			corporis obcaecati, unde aut, rerum sapiente magni libero minus
+			velit? Beatae minima est distinctio doloribus nemo illum iusto
+			veniam tenetur error consequatur placeat et sunt corporis
+			consequuntur ab eaque, sapiente fugit ad quos nobis obcaecati
+			recusandae? Ipsam ab possimus quae, sunt a natus. Corrupti quae
+			accusamus modi.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea eos
+			veritatis rerum. Vero, cupiditate itaque ratione non officia
+			consectetur iusto expedita impedit. Culpa officiis tempora nulla
+			excepturi, ipsum quos odio, harum cum cumque neque consequuntur
+			dicta obcaecati. Eaque totam adipisci dolore explicabo nulla nostrum
+			labore veritatis mollitia nobis dolorum ducimus, non ut vero
+			corporis obcaecati, unde aut, rerum sapiente magni libero minus
+			velit? Beatae minima est distinctio doloribus nemo illum iusto
+			veniam tenetur error consequatur placeat et sunt corporis
+			consequuntur ab eaque, sapiente fugit ad quos nobis obcaecati
+			recusandae? Ipsam ab possimus quae, sunt a natus. Corrupti quae
+			accusamus modi.
+		</p>
+	</div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<h1>Homepage</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Necessitatibus, culpa itaque. Consequuntur laboriosam nobis ab
+			dolorem qui quaerat ipsum. Reiciendis voluptatibus sapiente expedita
+			fugit quibusdam eveniet, reprehenderit impedit. Similique magnam,
+			inventore recusandae id consectetur minus accusamus? Ab impedit
+			magni ea eos, ut deleniti voluptas dignissimos quis vel vitae sed
+			ratione odit saepe dolorem optio asperiores consectetur culpa et
+			omnis earum, fugiat expedita molestias totam temporibus. Quis
+			aliquam voluptate libero officiis aperiam suscipit consequatur
+			nesciunt a vero sequi culpa quos, soluta, quod omnis earum. Officiis
+			reiciendis voluptas architecto corrupti iure quia natus omnis. Illum
+			molestias deleniti, eius dolor vero voluptates tempora!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Necessitatibus, culpa itaque. Consequuntur laboriosam nobis ab
+			dolorem qui quaerat ipsum. Reiciendis voluptatibus sapiente expedita
+			fugit quibusdam eveniet, reprehenderit impedit. Similique magnam,
+			inventore recusandae id consectetur minus accusamus? Ab impedit
+			magni ea eos, ut deleniti voluptas dignissimos quis vel vitae sed
+			ratione odit saepe dolorem optio asperiores consectetur culpa et
+			omnis earum, fugiat expedita molestias totam temporibus. Quis
+			aliquam voluptate libero officiis aperiam suscipit consequatur
+			nesciunt a vero sequi culpa quos, soluta, quod omnis earum. Officiis
+			reiciendis voluptas architecto corrupti iure quia natus omnis. Illum
+			molestias deleniti, eius dolor vero voluptates tempora!
+		</p>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'Home',
+	components: {}
+};
 </script>
