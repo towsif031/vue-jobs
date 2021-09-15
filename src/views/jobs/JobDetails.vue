97f8d7cdaf806db41b6a0b718d15eab38931dba5
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-16 00:56:26 +0600

    dynamic links.

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -5,11 +5,12 @@
 
 <script>
 export default {
-	data() {
-		return {
-			id: this.$route.params.id
-		};
-	}
+	props: ['id']
+	// data() {
+	// 	return {
+	// 		id: this.$route.params.id
+	// 	};
+	// }
 };
 </script>
 
