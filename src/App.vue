9f0528d1b460b9bc3f8416a0b87494e35a5a5f1e
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-16 01:39:48 +0600

    programmatic navigation.

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,29 @@
 		<router-link :to="{ name: 'About' }">About</router-link> |
 		<router-link :to="{ name: 'Jobs' }">Jobs</router-link>
 	</div>
+
+	<button @click="redirect">Redirect</button>
+	<button @click="back">Go Back</button>
+	<button @click="forward">Go Forward</button>
+
 	<router-view />
 </template>
+
+<script>
+export default {
+	methods: {
+		redirect() {
+			this.$router.push({ name: 'Home' });
+		},
+		back() {
+			this.$router.go(-1);
+		},
+		forward() {
+			this.$router.go(1);
+		}
+	}
+};
+</script>
 
 <style>
 #app {
@@ -31,5 +52,12 @@
 	color: #42b983;
 	border: 1px solid #42b983;
 	border-radius: 5px;
+}
+
+button {
+	margin: 0 10px;
+	padding: 10px;
+	border: none;
+	border-radius: 4px;
 }
 </style>
