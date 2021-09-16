f02c0b2ed8985399c7a6e3ebb97ecc57fdb00369
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-16 20:33:26 +0600

    conditionally showing data

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,7 +1,12 @@
 <template>
-	<h1>{{ job.title }}</h1>
-	<p>The job id is {{ id }}</p>
-	<p>{{ job.details }}</p>
+	<div v-if="job">
+		<h1>{{ job.title }}</h1>
+		<p>The job id is {{ id }}</p>
+		<p>{{ job.details }}</p>
+	</div>
+	<div v-else>
+		<p>Loading job details...</p>
+	</div>
 </template>
 
 <script>
@@ -13,7 +18,7 @@ export default {
 		};
 	},
 	mounted() {
-		fetch('http://localhost:3000/jobs' + this.id)
+		fetch('http://localhost:3000/jobs/' + this.id)
 			.then((res) => res.json())
 			.then((data) => (this.job = data))
 			.catch((err) => console.log(err.message));
