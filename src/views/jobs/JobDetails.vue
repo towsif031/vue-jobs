243475faf0248064f51194c7fdba73e822efbe9f
Towsif Ahmed Omi <towsif031@gmail.com>
2021-09-16 20:20:00 +0600

    fetching data from external api

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,23 @@
 <template>
-	<h1>Job Details Page</h1>
+	<h1>{{ job.title }}</h1>
 	<p>The job id is {{ id }}</p>
+	<p>{{ job.details }}</p>
 </template>
 
 <script>
 export default {
-	props: ['id']
-	// data() {
-	// 	return {
-	// 		id: this.$route.params.id
-	// 	};
-	// }
+	props: ['id'],
+	data() {
+		return {
+			job: null
+		};
+	},
+	mounted() {
+		fetch('http://localhost:3000/jobs' + this.id)
+			.then((res) => res.json())
+			.then((data) => (this.job = data))
+			.catch((err) => console.log(err.message));
+	}
 };
 </script>
 
